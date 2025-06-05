71973e9ba130dc0b1b1b83730214b500b41404fd
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2025-06-05 13:54:17 +0530

    Time: 60 ms (22.36%), Space: 113.4 MB (23.7%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,30 @@
+class RandomizedSet {
+private:
+    vector<int> a; 
+    unordered_map<int,int> m; 
+public:
+    RandomizedSet() {
+
+    }
+    
+    bool insert(int val) {
+        if(m.find(val)!=m.end()) return false;
+        a.push_back(val); 
+        m[val]=a.size()-1;
+        return true;
+    }
+
+    bool remove(int val) {
+        if(m.find(val)==m.end()) return false;
+        int last=a.back();     
+        a[m[val]]=a.back();                
+        a.pop_back();            
+        m[last]=m[val];
+        m.erase(val);            
+        return true;
+    }
+    
+    int getRandom() {
+        return a[rand()%a.size()];
+    }
+};
