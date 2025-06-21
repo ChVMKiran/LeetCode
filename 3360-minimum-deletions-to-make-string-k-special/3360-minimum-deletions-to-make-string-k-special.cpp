235592bc4787253dfbf09ae8b0ddc62cb21477b2
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2025-06-21 11:57:29 +0530

    Time: 18 ms (32.61%), Space: 19.6 MB (50%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumDeletions(string word, int k) {
+        unordered_map<int, int> ump;
+        for(char &ch: word) {
+            ump[ch]++;
+        }
+        int ans = word.size();
+        for(auto &[ele1, freq1] : ump) {
+            int curr = 0;
+            for(auto &[ele2, freq2] : ump) {
+                if(freq2 < freq1) curr += freq2;
+                else if(freq2 > freq1 + k) curr += (freq2 - freq1 - k);
+            }
+            ans = min(ans, curr);
+        }
+        return ans;
+    }
+};
