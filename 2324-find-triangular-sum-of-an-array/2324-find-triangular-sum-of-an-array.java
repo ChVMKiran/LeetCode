c60e5c4cf78a5b1cd67b8167ad8bfb38a40253f4
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2025-09-30 09:10:53 +0530

    Time: 25 ms (98.66%), Space: 45.1 MB (26.73%) - LeetHub

---
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+
+class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        BigInteger ans = new BigInteger("0");
+        BigInteger coeff = new BigInteger("1");
+        BigInteger MOD = new BigInteger("10");
+        for(int i = 0; i < n; i++) {
+            ans = ans.add(coeff.multiply(BigInteger.valueOf(nums[i])));
+            coeff = coeff.multiply(BigInteger.valueOf(n - i - 1)).divide(BigInteger.valueOf(i + 1));
+        }
+        return ans.mod(MOD).intValue();
+    }
+}
