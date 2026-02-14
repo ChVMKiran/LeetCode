fbc477ef32e8fa51975ec5cfa2b25d51d2983a9e
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-02-14 15:36:40 +0530

    Time: 219 ms (5.08%), Space: 8.6 MB (96.85%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double wine[100][100] = {0};
+        wine[0][0] = poured;
+        for(int i = 0; i < query_row; i++) {
+            for(int j = 0; j <= i; j++) {
+                wine[i + 1][j] += max(0.0, wine[i][j] - 1) / 2.0;
+                wine[i + 1][j + 1] += max(0.0, wine[i][j] - 1) / 2.0;
+            }
+        }
+        for(int i = 0; i <= query_row; i++) {
+            for(int j = 0; j <= i; j++) {
+                cout<<wine[i][j]<<' ';
+            }
+            cout<<'\n';
+        }
+        return min(1.0, wine[query_row][query_glass]);
+    }
+};
