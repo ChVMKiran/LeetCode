c2b58606fa4628dc77587683e8dd7c6df35d9150
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-02-16 09:13:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (46.07%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            ans *= 2;
+            if(n & 1) ans += 1;
+            n >>= 1;
+        }
+        return ans;
+    }
+};
