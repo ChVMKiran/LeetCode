472eb4d1f4646a622121b1f3052564c02a78118a
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-04-01 22:47:05 +0530

    Time: 146 ms (88.39%), Space: 38.9 MB (93.57%) - LeetHub

--- 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.cpp
+++ 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int ind, int target, vector<unordered_map<int, int>>& dp) {
-        if(ind == nums.size()) {
-            if(target == 0) return 0;
-            return -1;
+    int dp[41][40002];
+    int helper(int ind, int tar, int n, vector<int>& nums) {
+        if (ind == n) {
+            if (tar == 0)
+                return 0;
+            return -1e6;
         }
-        if(dp[ind].count(target)) return dp[ind][target];
-        int take = helper(nums, ind + 1, target ^ nums[ind], dp);
-        if(take != -1) take = take + 1;
-        int notTake = helper(nums, ind + 1, target, dp);
-        return dp[ind][target] = max(take, notTake);
+        if (dp[ind][tar] != -1) {
+            return dp[ind][tar];
+        }
+        int mx = -1e6;
+        mx = max(mx, 1 + helper(ind + 1, tar ^ nums[ind], n, nums));
+        mx = max(mx, helper(ind + 1, tar, n, nums));
+        return dp[ind][tar] = mx;
     }
 
     int minRemovals(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<unordered_map<int, int>> dp(n);
-        int res = helper(nums, 0, target, dp);
-        if(res == -1) return -1;
-        return n - res;
+        memset(dp, -1, sizeof(dp));
+        int res = helper(0, target, nums.size(), nums);
+        if (res < 0)
+            return -1;
+        return nums.size() - res;
     }
 };
