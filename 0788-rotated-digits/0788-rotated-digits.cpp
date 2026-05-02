45304268fa9c57a01682417c34351dc078611705
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-05-02 08:14:56 +0530

    Time: 0 ms (100%), Space: 9.7 MB (17.02%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int rotatedDigits(int n) {
-        int ans = 0;
-        for(int i = 1; i <= n; i++) {
-            int j = i;
-            bool changed = false, valid = true;
-            while(j) {
-                if(j % 10 == 2 || j % 10 == 5 || j % 10 == 6 || j % 10 == 9) {
-                    changed = true;
+        vector<int> dp(n + 1, 0);
+        int count = 0;
+
+        for(int i = 0; i <= n; i++){
+            if(i < 10){
+                if(i == 0 || i == 1 || i == 8) dp[i] = 1;
+                else if(i == 2 || i == 5 || i == 6 || i == 9){
+                    dp[i] = 2;
+                    count++;
                 }
-                else if(j % 10 == 3 || j % 10 == 4 || j % 10 == 7) {
-                    valid = false;
-                    break;
-                }
-                j /= 10;
+                else dp[i] = 0;
             }
-            if(changed & valid) ans++;
+            else{
+                int a = dp[i / 10];
+                int b = dp[i % 10];
+
+                if(a == 0 || b == 0) dp[i] = 0;
+                else if(a == 1 && b == 1) dp[i] = 1;
+                else {
+                    dp[i] = 2;
+                    count++;
+                }
+            }
         }
-        return ans;
+
+        return count;
     }
 };
