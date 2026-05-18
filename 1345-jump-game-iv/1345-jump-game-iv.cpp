c91cec557a1a6ba1c8c3d7b8bb7a46ab05c8388e
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-05-18 10:23:55 +0530

    Time: 146 ms (26.17%), Space: 101.2 MB (24.53%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -3,27 +3,28 @@ public:
     int minJumps(vector<int>& arr) {
         int n = arr.size();
         unordered_map<int, vector<int>> ump;
-        vector<int> cost(n, INT_MAX);
+        vector<bool> vis(n);
         for(int i = 0; i < n; i++) ump[arr[i]].push_back(i);
         queue<pair<int, int>> qu;
         qu.push({0, 0});
-        cost[0] = 0;
+        vis[0] = true;
         while(!qu.empty()) {
             auto [ind, cnt] = qu.front();
+            // cout<<ind<<' ';
             if(ind == n - 1) return cnt;
             qu.pop();
-            if(ind > 0 && cost[ind - 1] >= cnt + 1) {
+            if(ind > 0 && !vis[ind - 1]) {
                 qu.push({ind - 1, cnt + 1});
-                cost[ind - 1] = cnt + 1;
+                vis[ind - 1] = true;
             }
-            if(ind < n - 1 && cost[ind + 1] >= cnt + 1) {
+            if(ind < n - 1 && !vis[ind + 1]) {
                 qu.push({ind + 1, cnt + 1});
-                cost[ind + 1] = cnt + 1;
+                vis[ind + 1] = true;
             }
             for(auto &i: ump[arr[ind]]) {
-                if(cost[i] <= cnt + 1) continue;
+                if(vis[i]) continue;
                 qu.push({i, cnt + 1});
-                cost[i] = cnt + 1;
+                vis[i] = true;
             }
             ump.erase(arr[ind]);
         }
