fb8c83acc53d53742f49d0ded8f891189f34bbc8
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-05-30 21:23:08 +0530

    Time: 526 ms (44.55%), Space: 366.5 MB (41.58%) - LeetHub

---
+++ 3161-block-placement-queries/3161-block-placement-queries.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int n = 50000;
+    vector<int> segTree;
+    void update(int ind, int val, int l, int r, int i) {
+        if(l == r) {
+            segTree[i] = val;
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        if(ind <= mid) {
+            update(ind, val, l, mid, 2 * i + 1);
+        }
+        else {
+            update(ind, val, mid + 1, r, 2 * i + 2);
+        }
+        segTree[i] = max(segTree[2 * i + 1], segTree[2 * i + 2]);
+    }
+    int query(int start, int end, int l, int r, int i) {
+        if(start > r || end < l) return 0;
+        if(start <= l && r <= end) return segTree[i];
+        int mid = l + (r - l) / 2;
+        int left = query(start, end, l, mid, 2 * i + 1);
+        int right = query(start, end, mid + 1, r, 2 * i + 2);
+        return max(left, right);
+    }
+    vector<bool> getResults(vector<vector<int>>& queries) {
+        segTree.resize(4 * n);
+        set<int> st;
+        st.insert(0);
+        vector<bool> ans;
+        for(auto& q: queries) {
+            if(q[0] == 1) {
+                int x = q[1];
+                auto it = st.upper_bound(x);
+                int pre = *prev(it), nxt = (it != st.end()) ? *it: -1;
+                update(x, x - pre, 0, n - 1, 0);
+                if(nxt != -1) update(nxt, nxt - x, 0, n - 1, 0);
+                st.insert(x);
+            }
+            else {
+                auto it = st.upper_bound(q[1]);
+                int val = max(q[1] - *prev(it), query(0, *prev(it), 0, n - 1, 0));
+                ans.push_back(val >= q[2]);
+            }
+        }
+        return ans;
+    }
+};
