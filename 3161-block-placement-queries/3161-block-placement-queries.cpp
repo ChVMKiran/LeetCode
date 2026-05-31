020b5c710a1c088bcc1d2e215b598fd3059ed12b
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-05-31 18:52:39 +0530

    Time: 535 ms (41.12%), Space: 366.3 MB (41.12%) - LeetHub

--- 3161-block-placement-queries/3161-block-placement-queries.cpp
+++ 3161-block-placement-queries/3161-block-placement-queries.cpp
@@ -8,16 +8,12 @@ public:
             return;
         }
         int mid = l + (r - l) / 2;
-        if(ind <= mid) {
-            update(ind, val, l, mid, 2 * i + 1);
-        }
-        else {
-            update(ind, val, mid + 1, r, 2 * i + 2);
-        }
+        if(ind <= mid) update(ind, val, l, mid, 2 * i + 1);
+        else update(ind, val, mid + 1, r, 2 * i + 2);
         segTree[i] = max(segTree[2 * i + 1], segTree[2 * i + 2]);
     }
     int query(int start, int end, int l, int r, int i) {
-        if(start > r || end < l) return 0;
+        if(start > r || l > end) return 0;
         if(start <= l && r <= end) return segTree[i];
         int mid = l + (r - l) / 2;
         int left = query(start, end, l, mid, 2 * i + 1);
@@ -26,22 +22,23 @@ public:
     }
     vector<bool> getResults(vector<vector<int>>& queries) {
         segTree.resize(4 * n);
+        vector<bool> ans;
         set<int> st;
         st.insert(0);
-        vector<bool> ans;
         for(auto& q: queries) {
+            int x = q[1];
             if(q[0] == 1) {
-                int x = q[1];
                 auto it = st.upper_bound(x);
-                int pre = *prev(it), nxt = (it != st.end()) ? *it: -1;
+                int pre = *prev(it), nxt = (it != st.end()) ? *it : -1;
                 update(x, x - pre, 0, n - 1, 0);
                 if(nxt != -1) update(nxt, nxt - x, 0, n - 1, 0);
                 st.insert(x);
             }
             else {
-                auto it = st.upper_bound(q[1]);
-                int val = max(q[1] - *prev(it), query(0, *prev(it), 0, n - 1, 0));
-                ans.push_back(val >= q[2]);
+                auto it = st.upper_bound(x);
+                int pre = *prev(it);
+                int maxi = max(x - pre, query(0, pre, 0, n - 1, 0));
+                ans.push_back(maxi >= q[2]);
             }
         }
         return ans;
