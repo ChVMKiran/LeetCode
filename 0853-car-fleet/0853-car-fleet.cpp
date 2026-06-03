cbdf197a5715c4deefa8420dd41b1785ac359be4
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-06-03 11:49:52 +0530

    Time: 36 ms (82.64%), Space: 113.4 MB (35.96%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        priority_queue<pair<int, double>> pq;
+        vector<pair<int, double>> vec;
         int n = position.size(), ans = 0;
         for(int i = 0; i < n; i++) {
-            pq.push({position[i], (target - position[i]) / (speed[i] * 1.0)});
+            vec.push_back({position[i], (target - position[i]) / (speed[i] * 1.0)});
         }
-        while(!pq.empty()) {
-            auto [p, t] = pq.top();
-            // cout<<p<<' '<<t<<endl;
-            pq.pop();
-            while(!pq.empty() && pq.top().second <= t) {
-                // cout<<pq.top().first<<' '<<pq.top().second<<endl;
-                pq.pop();
+        sort(vec.begin(),vec.end());
+        while(!vec.empty()) {
+            auto [p, t] = vec.back();
+            vec.pop_back();
+            while(!vec.empty() && vec.back().second <= t) {
+                vec.pop_back();
             }
             ans++;
         }
