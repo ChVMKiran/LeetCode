32fb7c42c25e34e589e3aa12ba72d3fcdaf1e415
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-06-07 12:31:36 +0530

    Time: 128 ms (90.31%), Space: 259.3 MB (89.46%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        TreeNode* root;
+        int root = 0;
         unordered_map<int, TreeNode*> ump;
-        unordered_map<int, int> parent;
         for(auto& d: descriptions) {
-            if(!ump.count(d[0])) ump[d[0]] = new TreeNode(d[0]);
-            if(!ump.count(d[1])) ump[d[1]] = new TreeNode(d[1]);
+            if(!ump.count(d[0])) {
+                ump[d[0]] = new TreeNode(d[0]);
+                root ^= d[0];
+            }
+            if(!ump.count(d[1])) {
+                ump[d[1]] = new TreeNode(d[1]);
+                root ^= d[1];
+            }
+            root ^= d[1];
             if(d[2]) ump[d[0]]->left = ump[d[1]];
             else ump[d[0]]->right = ump[d[1]];
-            parent[d[1]] = d[0];
-            root = ump[d[0]];
         }
-        while(parent.count(root->val)) root = ump[parent[root->val]];
-        return root;
+        return ump[root];
     }
 };
