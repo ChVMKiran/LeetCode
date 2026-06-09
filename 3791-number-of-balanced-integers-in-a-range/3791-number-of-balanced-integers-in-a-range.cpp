cdb406f173ce8b594d4136c4117ea8c589d603a7
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-06-09 09:18:21 +0530

    Time: 31 ms (93.16%), Space: 10.3 MB (75.79%) - LeetHub

--- 3791-number-of-balanced-integers-in-a-range/3791-number-of-balanced-integers-in-a-range.cpp
+++ 3791-number-of-balanced-integers-in-a-range/3791-number-of-balanced-integers-in-a-range.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     using ll = long long;
-    ll dp[16][100][100][2];
+    ll dp[16][200][2];
     string s;
-    ll helper(int ind, int even, int odd, int tight) {
-        if(ind == s.size()) return even == odd;
-        if(dp[ind][even][odd][tight] != -1) return dp[ind][even][odd][tight];
+    ll helper(int ind, int diff, int tight) {
+        if(diff < 0 || diff >= 200) return 0;
+        if(ind == s.size()) return diff == 100;
+        if(dp[ind][diff][tight] != -1) return dp[ind][diff][tight];
         int limit = (tight ? s[ind] - '0' : 9);
         ll ans = 0;
         for(int i = 0; i <= limit; i++) {
-            ll newEven = even, newOdd = odd;
-            if(ind % 2 == 0) newEven += i;
-            else newOdd += i;
-            ll val = helper(ind + 1, newEven, newOdd, (tight && i == limit ? 1: 0));
-            ans += val;
+            ans += helper(ind + 1, diff + (ind % 2 ? i : -i), (tight && i == limit ? 1: 0));
         }
-        return dp[ind][even][odd][tight] = ans;
+        return dp[ind][diff][tight] = ans;
     }
     ll solve(ll num) {
         s = to_string(num);
         memset(dp, -1, sizeof(dp));
-        return helper(0, 0, 0, 1);
+        return helper(0, 100, 1);
     }
     ll countBalanced(ll low, ll high) {
         return solve(high) - solve(low - 1);
