a7aacdf7c4c39c7d96f6d277d892ca0fe51627f5
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-06-11 07:34:52 +0530

    Time: 315 ms (41.61%), Space: 333.3 MB (53.02%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        int n = edges.size() + 1, mod = 1e9 + 7, maxi = 0;
+        long ans = 1;
+        vector<vector<int>> adj(n + 1);
+        for(auto& e: edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        vector<bool> vis(n + 1);
+        queue<pair<int, int>> qu;
+        qu.push({1, 0});
+        vis[1] = 1;
+        while(!qu.empty()) {
+            auto [node, height] = qu.front();
+            // cout<<node<<' '<<height<<endl;
+            maxi = max(maxi, height);
+            qu.pop();
+            for(int& nn: adj[node]) {
+                if(!vis[nn]) {
+                    vis[nn] = 1;
+                    qu.push({nn, height + 1});
+                }
+            }
+        }
+        while(--maxi) {
+            ans <<= 1;
+            ans %= mod;
+        }
+        return ans;
+    }
+};
