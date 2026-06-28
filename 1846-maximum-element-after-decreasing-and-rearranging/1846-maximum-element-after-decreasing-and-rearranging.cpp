b5e5d43854419bcf2835a0f23d2ccd24832e024a
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-06-28 18:37:04 +0530

    Time: 0 ms (100%), Space: 57.4 MB (4.52%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        unordered_map<int, int> ump;
         int ans = 1, n = arr.size(), maxi = 1;
+        vector<int> ump(n + 1);
         for(int i = 0; i < n; i++)  ump[min(n, arr[i])]++;
         for(int i = 2; i <= n; i++) {
             ans = min(ans + ump[i], i);
