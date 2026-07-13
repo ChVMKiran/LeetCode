784ff4e9a97d5403056513452df3a6f9c4c72309
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-07-13 22:36:22 +0530

    Time: 0 ms (100%), Space: 8.2 MB (97.69%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
+    inline static int nums[45];
+    bool init = []() {
+        int n = 0;
+        for(int i = 1; i < 10; i++) nums[n++] = i;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] % 10 < 9) nums[n++] = nums[i] * 10 + nums[i] % 10 + 1;
+        }
+        return 0;
+    }();
     vector<int> sequentialDigits(int low, int high) {
         vector<int> ans;
-        for(int i = log10(low), ten = pow(10, i - 1); i <= log10(high); i++) {
-            int curr = 1, cnt = 1;
-            ten *= 10;
-            while(cnt <= i) curr = curr * 10 + curr % 10 + 1, cnt++;
-            while(curr % 10) {
-                if(curr <= high) {
-                    if(curr >= low) ans.push_back(curr);
-                }
-                else break;
-                curr = (curr % ten) * 10 + curr % 10 + 1;
-            }
+        for(int i = 0; i < 45; i++) {
+            if(nums[i] >= low && nums[i] <= high) ans.push_back(nums[i]);
         }
         return ans;
     }
