0a9e3ecce0f496553f6cfdaf8cd21f9a4a391427
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-08-01 16:06:17 +0530

    Time: 0 ms (100%), Space: 9.6 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
+    long long dp[20][20];
+    int n;
+    long long helper(int i, int j, vector<int>& nums) {
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(i == j) return dp[i][j] = nums[i];
+        long long res1 = nums[i] + min(helper(i + 1, j - 1, nums), helper(i + 2, j, nums));
+        long long res2 = nums[j] + min(helper(i, j - 2, nums), helper(i + 1, j - 1, nums));
+        return dp[i][j] = max(res1, res2);
+    }
     bool predictTheWinner(vector<int>& nums) {
-        map<vector<int>, bool> ump;
-        function<bool(int, int, int, int)> helper = [&](int i, int j, int turn, int sum) {
-            if(i > j) return sum >= 0;
-            if(ump.count({i, j, turn, sum})) return ump[{i, j, turn, sum}];
-            if(turn) {
-                return ump[{i, j, turn, sum}] = (
-                    helper(i + 1, j, !turn, sum - nums[i])
-                    && helper(i, j - 1, !turn, sum - nums[j])
-                );
-            }
-            else {
-                return ump[{i, j, turn, sum}] = (
-                    helper(i + 1, j, !turn, sum + nums[i])
-                    || helper(i, j - 1, !turn, sum + nums[j])
-                );
-            }
-        };
-        return helper(0, nums.size() - 1, 0, 0);
+        n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        long long maxSum = helper(0, n - 1, nums);
+        long long totalSum = accumulate(nums.begin(), nums.end(), 0);
+        return 2 * maxSum >= totalSum;
     }
 };
