e2dab41e9110cd7ac859e88e2304d45d5ad712c5
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2026-08-04 21:27:27 +0530

    Time: 0 ms (100%), Space: 32.1 MB (91.09%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        bitset<101> freq = 0;
         vector<int> ans;
-        int n = nums.size();
-        for(int i = 1; i < n; i++) {
-            if(nums[i] > nums[i - 1] + 1) {
-                for(int j = nums[i - 1] + 1; j < nums[i]; j++) ans.push_back(j);
-            }
+        int n = nums.size(), mn = 100, mx = 1;
+        for(int i = 0; i < n; i++) {
+            freq[nums[i]] = 1;
+            mn = min(mn, nums[i]);
+            mx = max(mx, nums[i]);
+        }
+        for(int i = mn; i < mx; i++) {
+            if(!freq[i]) ans.push_back(i);
         }
         return ans;
     }
